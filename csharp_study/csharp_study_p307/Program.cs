ed01174e6f9b88356f162c956ca88b493d8b6f55
kimsoosoo0928 <kimsoosoo0928@gmail.com>
2021-12-03 17:30:11 +0900

    test

---
+++ csharp_study/csharp_study_p307/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_study_p307
+{
+    class Program
+    {
+        interface ILogger
+        {
+            void writeLog(string message) : 
+        }
+
+        interface IFormattableLogger : ILogger
+        {
+            void WriteLog(string format, params Object[] args);
+        }
+        
+        class ConsoleLogger2 : IFormattableLogger
+        {
+            public void WriteLog(string message)
+            {
+                Console.WriteLine("{0} {1}", DateTime.Now.ToLocalTime(), message);
+            }
+            
+            public void WriteLog(string  format, params Object[] args)
+            {
+                String message = String.Format(format, args);
+                Console.WriteLine("{0}, {1}", DateTime.Now.ToLocalTime(), message);
+            }
+        }
+
+        class MainApp
+        {
+            static void Main(string[] args)
+            {
+                IFormattableLogger = new ConsoleLogger2();
+                logger.WriteLog("The world is not flat.");
+                ConsoleLogger2.WriteLog("{0} + {1} = {2}", 1, 1, 2)
+                
+            }
+        }
+    }
+}
